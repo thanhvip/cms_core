d899b98c05c4b5dd306bd9de4174cc8f89f17601
thanhtv <thanhtv@novaon.vn>
2016-12-05 14:14:54 +0700

    update

---
+++ server.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var port = process.env.PORT || 1993;
+app.use(express.static(__dirname + '/public'));
+server.listen(port);
+console.log('Server is running on '+ port);
+exports = module.exports=app;
